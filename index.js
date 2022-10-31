e331f23417b19e567220b8e27ead32cb835944a6
codethi <docencia.thiago@gmail.com>
2022-10-31 20:00:21 -0300

    ex03 - os node

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-const fs = require('fs') // common js
+const os = require("os");
 
-fs.writeFileSync('hello.txt', 'Hello Fs')
+console.log(os.platform());
+console.log(os.cpus());
+console.log(os.cpus().length);
+console.log(os.totalmem());// total de memoria em bytes
+console.log(os.totalmem() / 1024 / 1024 / 1024); // total de memoria em GB
